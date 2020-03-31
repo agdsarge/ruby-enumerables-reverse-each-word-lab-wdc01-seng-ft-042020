5fd2b9b1a28beaa988cbadd89858c5a13f3345e1
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-31 18:06:47 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,15 +3,11 @@ require "pp"
 
 def reverse_each_word(sentence)
   
-  back_sent = ""
   split_sent = sentence.split()
   back_ar = split_sent.collect {|word| word.reverse}
-  
-  p back_ar
+
   new_sent = back_ar.join(" ")
-  back_ar.each {|word| back_sent += word}
-  puts ">"
-  p back_sent
+
   p new_sent
   return  new_sent
 end
